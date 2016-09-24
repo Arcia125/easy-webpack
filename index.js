28cdadf037fc78b004dbf674a0d5b008c800f93b
Arcia125 <tauren125@gmail.com>
2016-09-24 11:01:31 -0500

    fix bug that caused scripts to run in wrong directory

--- index.js
+++ index.js
@@ -3,13 +3,18 @@
 const chalk = require('chalk');
 const fs = require('fs');
 const util = require('util');
-const format = util.format;
+const path = require('path');
 
+const format = util.format;
 const exec = require('child_process').exec;
 
-var npmInit = 'npm init -f';
+const args = process.argv;
+const log = console.log;
+const cd = process.cwd();
 
-var devDeps = [
+
+// npm dev dependencies
+let devDeps = [
 'babel-core',
 'babel-loader',
 'babel-preset-es2015',
@@ -26,30 +31,33 @@ var devDeps = [
 'webpack-dev-server'
 ];
 
-var installDevDeps = format('npm install --save %s', devDeps.join(" "));
 
-var deps = [
+// npm dependencies
+let deps = [
 'react',
 'react-dom'
-]
-var installDeps = format('npm install --save %s', deps.join(" "))
+];
 
-const args = process.argv;
+// commands
+let npmInit = 'npm init -f';
+let gitInit = 'git init';
+let installDevDeps = format('npm install --save-dev %s', devDeps.join(" "));
+let installDeps = format('npm install --save %s', deps.join(" "));
 
-const log = console.log;
-
+let commands = [installDevDeps, installDeps, gitInit];
 
 function success(msg) {
 	log(chalk.green(msg));
 }
 
-function makeDir(path, mask, cb) {
+function makeDir(filepath, mask, cb) {
 	// allow mask to be optional
 	if (typeof mask == 'function') {
         cb = mask;
         mask = 0777;
     }
-    fs.mkdir(path, mask, (err) => {
+    //make the directory
+    fs.mkdir(filepath, mask, (err) => {
     	if (err) {
     		if (err.code == 'EEXIST'){
     			cb(null);
@@ -61,19 +69,36 @@ function makeDir(path, mask, cb) {
     		cb(null);
     	}
     });
-
 }
+
 function makeDirectory(folderName) {
-	makeDir(__dirname + folderName, (err) => {
+	makeDir(path.join(cd, folderName), (err) => {
 		if(err) {
 			log(chalk.yellow(error));
 		} else {
-			success(format('folder %s made in %s', folderName, __dirname));
+			success(format('folder %s made in %s', folderName, cd));
 		}
 	});
 }
+
+function initCommand(command, callback) {
+	exec(command, {cwd: cd}, (error, stdout, stderr) => {
+		if (error) {
+			log('error with init: ', error);
+			process.exit(1);
+		}
+		if (stdout) {
+			success(stdout);
+			callback();
+		}
+		if (stderr) {
+			log(chalk.red(stderr));
+		}
+	});
+}
+
 function runCommand(command) {
-	exec(command, (error, stdout, stderr) => {
+	exec(command, {cwd: cd}, (error, stdout, stderr) => {
 		if (error){
 			log('error', error);
 		}
@@ -81,16 +106,22 @@ function runCommand(command) {
 			success(stdout);
 		}
 		if (stderr) {
-			log(chalk.orange(stderr));
+			log(chalk.red(stderr));
 		}
 	});
 }
 
+function runAllCommands() {
+	commands.forEach(runCommand);
+}
+
+function firstCommit() {
+	runCommand(gitCommit);
+}
+
+
+
+
+initCommand(npmInit, runAllCommands);
 
 makeDirectory('src');
-
-runCommand(npmInit);
-
-runCommand(installDevDeps);
-
-runCommand(installDeps);
